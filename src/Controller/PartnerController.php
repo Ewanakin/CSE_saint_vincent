929c277086be17bbcf932b13ed6c802c2ceddcc8
Ewan Quioc <quioc.ewan@gmail.com>
2023-03-17 21:34:26 +0100

    ajout de la focntion liste des partner, ajout d'un partner, modification d'un partner, suppression d'un partner.

---
+++ src/Controller/PartnerController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Partner;
+use App\Form\PartnerType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PartnerController extends AbstractController
+{
+    #[Route('/partner', name: 'partner_list')]
+    public function partnerList(ManagerRegistry $em): Response
+    {
+
+        $partners = $em->getRepository(Partner::class)->findAll();
+        return $this->render('partner/index.html.twig', [
+            'partners' => $partners,
+        ]);
+    }
+
+    #[Route('/partner/ajout', name: 'partner_add')]
+    #[Route('/partner/modification/{partner}', name: 'partner_edit')]
+    public function partnerEdit(EntityManagerInterface $em, Partner $partner = null, Request $request): Response
+    {
+        if($partner==null)
+        {
+            $partner = new Partner();
+        }
+        $formPartner = $this->createForm(PartnerType::class, $partner);
+        $formPartner->handleRequest($request);
+        if($formPartner->isSubmitted() && $formPartner->isValid())
+        {
+            $partner = $formPartner->getData();
+            $em->persist($partner);
+            $em->flush();
+            return $this->redirectToRoute("partner_list");
+        }
+        return $this->render('partner/edit.html.twig', [
+            'partnerForm' => $formPartner->createView(),
+            'partner' => $partner,
+        ]);
+    }
+
+    #[Route('/partner/supprimer/{partner}', name: 'partner_delete')]
+    public function partnerDelete(EntityManagerInterface $em, Partner $partner): Response
+    {
+        $em->remove($partner);
+        $em->flush();
+        return $this->redirectToRoute("partner_list");
+    }
+}
