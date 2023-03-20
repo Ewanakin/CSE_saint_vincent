cf46dac7d24a4754f1c2fc66a3de374666062ff1
Ewan Quioc <quioc.ewan@gmail.com>
2023-03-20 17:26:47 +0100

    ajout du sondaga avec crud reponses/questions

---
+++ src/Entity/Survey.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SurveyRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SurveyRepository::class)]
+class Survey
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $clientReponse = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getClientReponse(): ?int
+    {
+        return $this->clientReponse;
+    }
+
+    public function setClientReponse(int $clientReponse): self
+    {
+        $this->clientReponse = $clientReponse;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTime
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTime $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
