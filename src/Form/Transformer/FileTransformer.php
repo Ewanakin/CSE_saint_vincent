ab1a44a307692c70d0570e56d9b6bb8e1015792b
Ewan Quioc <quioc.ewan@gmail.com>
2023-03-27 08:12:33 +0200

    modif transformer.

--- src/Form/Transformer/FileTransformer.php
+++ src/Form/Transformer/FileTransformer.php
@@ -10,7 +10,7 @@ class FileTransformer implements DataTransformerInterface
 {
     public function transform($linkPicture)
     {
-        return $linkPicture;
+        return $linkPicture;        
     }
 
     public function reverseTransform($picture)
