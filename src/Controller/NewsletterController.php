d164a10414314d1a7ad486beacd029c03405b8b0
Ewan Quioc <quioc.ewan@gmail.com>
2023-03-31 20:54:20 +0200

    ajout du désabonnement au newsletter .

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -13,15 +13,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class NewsletterController extends AbstractController
 {
     #[Route('/newsletter/add', name: 'newsletter_add')]
+    #[Route('/newsletter/delete', name:"newsletter_delete")]
     public function index(Request $request, EntityManagerInterface $em): Response
     {
         $newsletter = new Newsletter();
         $newsletterForm = $this->createForm(NewsletterType::class, $newsletter);
         $newsletterForm->handleRequest($request);
+        $routeName = $request->attributes->get('_route');
         if($newsletterForm->isSubmitted() && $newsletterForm->isValid())
         {
             $newsletter = $newsletterForm->getData();
-            $em->persist($newsletter);
+            if($routeName=='newsletter_delete')
+            {
+                $newsletter = $em->getRepository(Newsletter::class)->findOneBy(array('email'=>$newsletter->getEmail()));
+                $em->remove($newsletter);
+            }
+            else
+            {
+                $em->persist($newsletter);
+            }
             $em->flush();
             return $this->redirectToRoute('newsletter_add');
         }
